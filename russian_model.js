6ac7eb9b0740c51899c16a2578517549132c9bb4
DavidStewartLDN <davidstewart.work@gmail.com>
2020-05-19 19:27:56 +0100

    created model for russian language

--- russian_model.js
+++ russian_model.js
@@ -6,3 +6,42 @@ const pool = new Pool({
   password: 'root',
   port: 5432,
 });
+
+const getRussian = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM russian ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+const createRussian = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { name, email } = body
+    pool.query('INSERT INTO russian (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new merchant has been added added: ${results.rows[0]}`)
+    })
+  })
+}
+const deleteRussian = () => {
+  return new Promise(function(resolve, reject) {
+    const id = parseInt(request.params.id)
+    pool.query('DELETE FROM russian WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`Merchant deleted with ID: ${id}`)
+    })
+  })
+}
+
+module.exports = {
+  getRussian,
+  createRussian,
+  deleteRussian,
+}
