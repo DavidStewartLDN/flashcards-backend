5e2c810a5d3cc7f50fe9da897f5b4eff157bf3d0
DavidStewartLDN <davidstewart.work@gmail.com>
2020-05-20 10:42:29 +0100

    corrected deleteRussian function to correctly take id of deletion row

--- russian_model.js
+++ russian_model.js
@@ -28,9 +28,8 @@ const createRussian = (body) => {
     })
   })
 }
-const deleteRussian = () => {
+const deleteRussian = (id) => {
   return new Promise(function(resolve, reject) {
-    const id = parseInt(request.params.id)
     pool.query('DELETE FROM russian WHERE id = $1', [id], (error, results) => {
       if (error) {
         reject(error)
